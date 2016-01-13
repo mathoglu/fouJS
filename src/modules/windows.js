fce0d39135cd6eb0c42441d32f690c089253e1bf
Matts Höglund <matts.hoglund@netlight.com>
2016-01-13 14:46:47 +0200

    Added new windows

--- src/modules/windows.js
+++ src/modules/windows.js
@@ -1,4 +1,10 @@
+import {validation} from './validation.js';
+
 let hann = (size)=> {
+		validation.addRules({
+			size: 'int'
+		})({size: size});
+
 		let table = new Float32Array(size),
 			cos = Math.cos,
 			twoPi = Math.PI*2;
@@ -10,18 +16,64 @@ let hann = (size)=> {
 		return (i)=> { return table[i] };
 	};
 
+let hamming = (size)=> {
+
+	validation.addRules({
+		size: 'int'
+	})({size: size});
+
+	let alpha = 0.54,
+		beta = 1-alpha,
+		table = new Float32Array(size),
+		cos = Math.cos,
+		twoPi = Math.PI*2;
+
+	for(let i = 0; i < size; i++) {
+		table[i] = alpha - beta*cos( twoPi*i / size-1 );
+	}
+	return (i)=> { return table[i] };
+};
+
 let blackman = (size)=> {
-		let table = new Float32Array(size),
+
+		validation.addRules({
+			size: 'int'
+		})({size: size});
+
+		let alpha = 0.16,
+			a0 = 1-alpha/2,
+			a1 = 0.5,
+			a2 = alpha/2,
+			table = new Float32Array(size),
 			cos = Math.cos,
 			twoPi = Math.PI*2;
 
 		for(let i = 0; i < size; i++) {
-
+			table[i] = a0 - a1*cos( twoPi*i / size-1 ) + a2*cos( 2*twoPi*i / size-1 );
 		}
 		return (i)=> { return table[i] };
 	};
 
+let bartlett = ()=> {
+	validation.addRules({
+		size: 'int'
+	})({size: size});
+
+	let table = new Float32Array(size);
+
+	for(let i = 0; i < size/2; i++) {
+		table[i] = 2*n / size;
+	}
+	for(let i = size/2 ;i < size; i++) {
+		table[i] = 2 - ( 2*n / size);
+	}
+
+	return (i)=> { return table[i] };
+};
+
 export default {
 	hann,
-	blackman
+	blackman,
+	bartlett,
+	hamming
 }
